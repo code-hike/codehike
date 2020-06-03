302a8d85b93ca19e20313974ca7c7c6fb3036ba1
Rodrigo Pombo <pombopombopombo@gmail.com>
2020-06-02 21:52:20 -0300

    Responsive scaling

--- src/demo-hooks-talk/index.js
+++ src/demo-hooks-talk/index.js
@@ -25,46 +25,49 @@ export function Demo() {
   return (
     <>
       <CodeHikeHead title="Hooks Talk Demo | Code Hike" />
+      <GenerateScaleVar />
       <div className={s.page}>
-        <div className={s.pageTop} />
-        <div className={s.main}>
-          <div className={s.editor}>
-            <MiniEditor
-              progress={progress}
-              steps={editorSteps}
-              style={{ height: "100%" }}
-            />
-          </div>
-          <div className={s.rightCol}>
-            <div className={s.browser}>
-              <MiniBrowser
-                url="http://localhost:3000/"
+        <main>
+          <div className={s.pageTop} />
+          <div className={s.content}>
+            <div className={s.editor}>
+              <MiniEditor
+                progress={progress}
+                steps={editorSteps}
                 style={{ height: "100%" }}
-                children={browserSteps[state.currentIndex]}
               />
             </div>
-            <div className={s.player}>
-              <Player
-                videoId="dpw9EHDh2bM"
-                style={{ height: "100%" }}
-                steps={playerSteps}
-                stepIndex={state.currentIndex}
-                stepProgress={state.stepProgress}
-                isPlaying={state.isPlaying}
-                onChange={({ stepIndex, stepProgress, isPlaying }) =>
-                  setState(() => ({
-                    isPlaying,
-                    currentIndex: stepIndex,
-                    stepProgress,
-                  }))
-                }
-              />
+            <div className={s.rightCol}>
+              <div className={s.browser}>
+                <MiniBrowser
+                  url="http://localhost:3000/"
+                  style={{ height: "100%" }}
+                  children={browserSteps[state.currentIndex]}
+                />
+              </div>
+              <div className={s.player}>
+                <Player
+                  videoId="dpw9EHDh2bM"
+                  style={{ height: "100%" }}
+                  steps={playerSteps}
+                  stepIndex={state.currentIndex}
+                  stepProgress={state.stepProgress}
+                  isPlaying={state.isPlaying}
+                  onChange={({ stepIndex, stepProgress, isPlaying }) =>
+                    setState(() => ({
+                      isPlaying,
+                      currentIndex: stepIndex,
+                      stepProgress,
+                    }))
+                  }
+                />
+              </div>
             </div>
           </div>
-        </div>
-        <div className={s.pageBottom}>
-          <Footer />
-        </div>
+          <div className={s.pageBottom}>
+            <Footer />
+          </div>
+        </main>
       </div>
     </>
   );
@@ -104,4 +107,21 @@ function Footer() {
       </div>
     </footer>
   );
+}
+
+function GenerateScaleVar() {
+  const js = `
+(function() {
+  function resetScale() {
+    const M = 24;
+    const W = 1024 
+    const H = (W - M * 2) / 1.618 + M * 5;
+    const root = document.documentElement;
+    const scale = Math.min(1, root.clientWidth / W, root.clientHeight / H);
+    root.style.setProperty('--scale', scale);
+  }
+  resetScale()
+  window.addEventListener('resize', resetScale)
+})()`;
+  return <script dangerouslySetInnerHTML={{ __html: js }} />;
 }
