2f1dd319afd6e0656e848ba4552a37f67d79d0c2
Rodrigo Pombo <pombopombopombo@gmail.com>
2020-06-05 14:53:04 -0300

    Add scroller

--- src/demo-svelte-tutorial/index.js
+++ src/demo-svelte-tutorial/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { CodeHikeHead } from "src/code-hike-head";
 import s from "./index.module.css";
+import { Step, Scroller } from "packages/scroller/scroller";
 
 export { Demo };
 
 function Demo() {
+  const [index, setIndex] = React.useState(0);
   return (
     <>
       <CodeHikeHead title="Svelte Tutorial Demo | Code Hike" />
@@ -13,14 +15,16 @@ function Demo() {
         <main>
           <h1>Mini Svelte Tutorial</h1>
           <div className={s.content}>
-            <div className={s.text}>
-              <section>Something something</section>
-              <section>Something something</section>
-              <section>Something something</section>
-              <section>Something something</section>
-            </div>
+            <Scroller onStepChange={setIndex}>
+              <div className={s.text}>
+                <Step index={0}>Something something 0</Step>
+                <Step index={1}>Something something 1</Step>
+                <Step index={2}>Something something 2</Step>
+                <Step index={3}>Something something 3</Step>
+              </div>
+            </Scroller>
             <div className={s.stickerContainer}>
-              <div className={s.sticker}>Sticker</div>
+              <div className={s.sticker}>{index}</div>
             </div>
           </div>
           <footer>Foo Bar</footer>
