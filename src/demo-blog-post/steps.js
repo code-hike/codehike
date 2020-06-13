bc82a6d2b11aec1b5c5cdf3665a24d14361f863d
Rodrigo Pombo <pombopombopombo@gmail.com>
2020-06-13 17:25:25 -0300

    Add more steps

--- src/demo-blog-post/steps.js
+++ src/demo-blog-post/steps.js
@@ -32,78 +32,91 @@ const files = {
   },
   "05.index.js": {
     code: require("!!raw-loader!./steps/05.index.js").default,
+    tabs: ["package.json", "pages/index.js"],
     lang: "jsx",
     file: "pages/index.js",
     link: pr(3),
   },
   "06.0.index.js": {
     code: require("!!raw-loader!./steps/06.0.index.js").default,
+    tabs: ["package.json", "pages/index.js"],
     lang: "jsx",
     file: "pages/index.js",
     link: pr(4),
   },
   "07.index.js": {
     code: require("!!raw-loader!./steps/07.index.js").default,
+    tabs: ["package.json", "pages/index.js"],
     lang: "jsx",
     file: "pages/index.js",
     link: pr(5),
   },
   "08.index.js": {
     code: require("!!raw-loader!./steps/08.index.js").default,
+    tabs: ["package.json", "pages/index.js"],
     lang: "jsx",
     file: "pages/index.js",
     link: pr(6),
   },
   "09.0.index.js": {
     code: require("!!raw-loader!./steps/09.0.index.js").default,
+    tabs: ["package.json", "pages/index.js"],
     lang: "jsx",
     file: "pages/index.js",
     link: pr(22),
   },
   "11.index.js": {
     code: require("!!raw-loader!./steps/11.index.js").default,
+    tabs: ["package.json", "pages/index.js"],
     lang: "jsx",
     file: "pages/index.js",
     link: pr(23),
   },
   "12.index.js": {
     code: require("!!raw-loader!./steps/12.index.js").default,
+    tabs: ["package.json", "pages/index.js"],
     lang: "jsx",
     file: "pages/index.js",
     link: pr(24),
   },
   "13.0.index.js": {
     code: require("!!raw-loader!./steps/13.0.index.js").default,
+    tabs: ["package.json", "pages/index.js"],
     lang: "jsx",
     file: "pages/index.js",
     link: pr(25),
   },
   "13.1.index.js": {
     code: require("!!raw-loader!./steps/13.1.index.js").default,
+    tabs: ["package.json", "pages/index.js"],
     lang: "jsx",
     file: "pages/index.js",
     link: pr(26),
   },
   "14.country.js": {
     code: require("!!raw-loader!./steps/14.country.js").default,
+    tabs: ["pages/index.js", "pages/country/[name].js"],
     lang: "jsx",
     file: "pages/country/[name].js",
     link: pr(27),
   },
   "15.country.js": {
     code: require("!!raw-loader!./steps/15.country.js").default,
+    tabs: ["pages/index.js", "pages/country/[name].js"],
     lang: "jsx",
     file: "pages/country/[name].js",
     link: pr(28),
   },
   "16.country.js": {
     code: require("!!raw-loader!./steps/16.country.js").default,
+    tabs: ["pages/index.js", "pages/country/[name].js"],
     lang: "jsx",
     file: "pages/country/[name].js",
     link: pr(29),
   },
   "17.country.js": {
     code: require("!!raw-loader!./steps/17.country.js").default,
+    tabs: ["pages/index.js", "pages/country/[name].js"],
     lang: "jsx",
     file: "pages/country/[name].js",
     link: pr(30),
@@ -152,6 +165,78 @@ const steps = p(
     "06",
     <MiniEditor step={{ ...files["05.index.js"], focus: "13[80],18:28" }} />,
     <MiniBrowser url="https://ssg-demo-6840hkb70.now.sh/" />
+  ),
+  s(
+    "07",
+    <MiniEditor step={{ ...files["06.0.index.js"] }} />,
+    <MiniBrowser url="https://ssg-demo-98dxl0cve.now.sh/" />
+  ),
+  s(
+    "08",
+    <MiniEditor step={{ ...files["07.index.js"] }} />,
+    <MiniBrowser url="https://ssg-demo-hhhht6een.now.sh/" />
+  ),
+  s(
+    "09",
+    <MiniEditor step={{ ...files["08.index.js"] }} />,
+    <MiniBrowser url="https://ssg-demo-hhhht6een.now.sh/" />
+  ),
+  s(
+    "10",
+    <MiniEditor step={{ ...files["09.0.index.js"] }} />,
+    <MiniBrowser url="https://ssg-demo-jaj3z785n.now.sh/" />
+  ),
+  s(
+    "11",
+    <MiniEditor step={{ ...files["11.index.js"] }} />,
+    <MiniBrowser url="https://ssg-demo-jaj3z785n.now.sh/" />
+  ),
+  s(
+    "12",
+    <MiniEditor step={{ ...files["12.index.js"] }} />,
+    <MiniBrowser url="https://ssg-demo-7o3u1aqb3.now.sh/" />
+  ),
+  s(
+    "13",
+    <MiniEditor step={{ ...files["13.0.index.js"], focus: "34:41,48" }} />,
+    <MiniBrowser url="https://ssg-demo-kgxev2x16.now.sh/" />
+  ),
+  s(
+    "14",
+    <MiniEditor
+      step={{ ...files["13.1.index.js"], focus: "33,38:42,44,45" }}
+    />,
+    <MiniBrowser url="https://ssg-demo-kgxev2x16.now.sh/" />
+  ),
+  s(
+    "15",
+    <MiniEditor step={{ ...files["14.country.js"] }} />,
+    <MiniBrowser url="https://ssg-demo-klnt3u1pr.now.sh/country/Iran" />
+  ),
+  s(
+    "16",
+    <MiniEditor step={{ ...files["15.country.js"], focus: "2:17" }} />,
+    <MiniBrowser url="https://ssg-demo-klnt3u1pr.now.sh/country/Iran" />
+  ),
+  s(
+    "17",
+    <MiniEditor step={{ ...files["15.country.js"], focus: "13,19:24" }} />,
+    <MiniBrowser url="https://ssg-demo-fnqndl6fp.now.sh/country/Iran" />
+  ),
+  s(
+    "18",
+    <MiniEditor step={{ ...files["16.country.js"], focus: "19:27" }} />,
+    <MiniBrowser url="https://ssg-demo-klnt3u1pr.now.sh/country/Iran" />
+  ),
+  s(
+    "19",
+    <MiniEditor step={{ ...files["17.country.js"] }} />,
+    <MiniBrowser url="https://ssg-demo-nvk2rcogx.now.sh/country/Iran" />
+  ),
+  s(
+    "20",
+    <MiniBrowser url="https://nextjs-static-props.now.sh/" sandbox="" />,
+    <Box1 height={100} />
   )
 );
 
