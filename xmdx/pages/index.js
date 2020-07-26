0a6ff09261450fb3d49f133a6f138600b6166c2d
Rodrigo Pombo <pombopombopombo@gmail.com>
2020-07-26 15:14:30 -0300

    Add range

--- xmdx/pages/index.js
+++ xmdx/pages/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import s from "./index.module.css";
 import { MiniEditor } from "@code-hike/mini-editor";
 import { MiniBrowser } from "@code-hike/mini-browser";
+import { Range, getTrackBackground } from "react-range";
 
 const code = require("!!raw-loader!./content/scroller.mdx")
   .default;
@@ -39,6 +40,7 @@ export default function Page() {
             <Author />
           </div>
         </div>
+        <VideoControls />
       </main>
     </div>
   );
@@ -67,12 +69,64 @@ function Author() {
           right: 0,
         }}
       >
-        @pomber
-        <br />
-        Rodrigo Pombo
-        <br />
-        Foo Conf
+        <div>
+          <span style={{ fontSize: "1.2em" }}>@pomber</span>
+          <div style={{ height: 5 }} />
+          <span style={{ fontSize: "1.8em" }}>Rodrigo</span>
+          <br />
+          <span style={{ fontSize: "2em" }}>Pombo</span>
+          <div style={{ height: 10 }} />
+          <span style={{ fontSize: "1.3em" }}>
+            FOO CONF
+          </span>
+        </div>
       </div>
     </div>
+  );
+}
+
+const MIN = 0;
+const MAX = 100;
+
+function VideoControls() {
+  const [value, setValue] = React.useState(10);
+  return (
+    <Range
+      step={0.1}
+      min={MIN}
+      max={MAX}
+      values={[value]}
+      onChange={(values) => setValue(values[0])}
+      renderTrack={({ props, children }) => (
+        <div
+          {...props}
+          style={{
+            ...props.style,
+            height: "5px",
+            width: "100%",
+            background: getTrackBackground({
+              values: [value],
+              colors: ["red", "#ccc"],
+              min: MIN,
+              max: MAX,
+            }),
+          }}
+        >
+          {children}
+        </div>
+      )}
+      renderThumb={({ props }) => (
+        <div
+          {...props}
+          style={{
+            ...props.style,
+            height: "15px",
+            width: "15px",
+            borderRadius: "50%",
+            backgroundColor: "red",
+          }}
+        />
+      )}
+    />
   );
 }
