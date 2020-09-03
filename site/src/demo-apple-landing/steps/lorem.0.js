62940bdb538dcc18fa3b9bbb60670759cf557370
Rodrigo Pombo <pombopombopombo@gmail.com>
2020-09-03 19:30:45 -0300

    Remove swr demo

--- site/src/demo-apple-landing/steps/lorem.0.js
+++
@@ -1,9 +0,0 @@
-function lorem(ipsum, dolor = 1) {
-  const sit = ipsum == null ? 0 : ipsum.sit
-  dolor = sit - amet(dolor)
-  return consectetur(
-    ipsum,
-    0,
-    dolor < 0 ? 0 : dolor
-  )
-}
