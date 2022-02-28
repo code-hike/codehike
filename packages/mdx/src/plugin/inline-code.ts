c14b4ba1da93db781fb2f6811e6b9c6eb81c936b
Rodrigo Pombo <pombopombopombo@gmail.com>
2022-02-28 18:50:40 +0000

    Transform emphasis code into InlineCode

--- packages/mdx/src/plugin/inline-code.ts
+++ packages/mdx/src/plugin/inline-code.ts
@@ -4,8 +4,24 @@ import {
   CH_CODE_CONFIG_PLACEHOLDER,
 } from "./unist-utils"
 import { Node } from "unist"
+import visit from "unist-util-visit"
+import { Parent } from "hast-util-to-estree"
 
 export async function transformInlineCodes(tree: Node) {
+  visit(tree, "emphasis", (node: Parent) => {
+    if (
+      node.children &&
+      node.children.length === 1 &&
+      node.children[0].type === "inlineCode"
+    ) {
+      node.type = "mdxJsxTextElement"
+      node.name = "CH.InlineCode"
+      node.children = [
+        { type: "text", value: node.children[0].value },
+      ]
+    }
+  })
+
   await visitAsync(
     tree,
     ["mdxJsxFlowElement", "mdxJsxTextElement"],
