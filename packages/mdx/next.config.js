edd2d995da3ed93b4752760e3a9538086bd3b54a
Rodrigo Pombo <pombopombopombo@gmail.com>
2022-03-27 22:03:12 +0000

    And again

--- packages/mdx/next.config.js
+++ packages/mdx/next.config.js
@@ -1,16 +1,4 @@
-const { remarkCodeHike } = require("@code-hike/mdx")
-const theme = require("shiki/themes/nord.json")
-
-const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [[remarkCodeHike, { theme }]],
-  },
-})
-
-module.exports = withMDX({
-  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
-
+module.exports = {
   redirects() {
     return [
       {
@@ -20,4 +8,4 @@ module.exports = withMDX({
       },
     ]
   },
-})
+}
