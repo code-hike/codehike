c8767893118517e6bf70afe1468b1222c3c1f5dd
Rodrigo Pombo <pombopombopombo@gmail.com>
2022-03-27 21:46:09 +0000

    Ditch mdx-bundler

---
+++ packages/mdx/next.config.js
@@ -0,0 +1,23 @@
+const { remarkCodeHike } = require("@code-hike/mdx")
+const theme = require("shiki/themes/nord.json")
+
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [[remarkCodeHike, { theme }]],
+  },
+})
+
+module.exports = withMDX({
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/test",
+        permanent: false,
+      },
+    ]
+  },
+})
---
+++ packages/mdx/pages/[name].tsx
@@ -0,0 +1,95 @@
+import { compile, runSync } from "@mdx-js/mdx"
+import * as runtime from "react/jsx-runtime.js"
+import fs from "fs"
+import { remarkCodeHike } from "../src/index"
+import { CH } from "../src/components"
+import theme from "shiki/themes/slack-dark.json"
+import Link from "next/link"
+
+export async function getServerSideProps({ params }) {
+  const files = await fs.promises.readdir("./tests/")
+
+  const tests = files
+    .filter(filename => filename.endsWith(".mdx"))
+    .map(filename => filename.slice(0, -4))
+
+  const { name = "test" } = params
+  const mdxSource = await fs.promises.readFile(
+    `./tests/${name}.mdx`,
+    "utf8"
+  )
+  try {
+    const props = {
+      tests,
+      current: name,
+      code: String(
+        await compile(mdxSource, {
+          outputFormat: "function-body",
+          remarkPlugins: [
+            [remarkCodeHike, { autoImport: false, theme }],
+          ],
+        })
+      ),
+    }
+    return { props }
+  } catch (e) {
+    console.error(
+      "getServerSideProps error",
+      JSON.stringify(e, null, 2)
+    )
+  }
+  return { props: { error: true, tests, current: name } }
+}
+
+export default function Page({ current, code, tests }) {
+  const { default: Content } = runSync(code, runtime)
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        gap: 8,
+        margin: "8px",
+      }}
+    >
+      <nav
+        style={{
+          background: "#fafafa",
+          borderRadius: 4,
+          padding: 16,
+          minWidth: 180,
+        }}
+      >
+        <ul style={{ margin: 0, padding: 0 }}>
+          {tests.map(test => (
+            <li key={test} style={{ listStyle: "none" }}>
+              <Link href={`/${encodeURIComponent(test)}`}>
+                <a
+                  style={{
+                    color: "black",
+                    textDecoration: "none",
+                    fontWeight:
+                      test === current ? "bold" : "normal",
+                  }}
+                >
+                  {test}
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <div
+        style={{
+          maxWidth: 900,
+          minWidth: 600,
+          background: "#fafafa",
+          borderRadius: 4,
+          padding: 16,
+        }}
+      >
+        <Content components={{ CH }} />
+      </div>
+    </div>
+  )
+}
