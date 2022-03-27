d029668a7b827d357d78578a48cb650e0e6e0a59
Rodrigo Pombo <pombopombopombo@gmail.com>
2022-03-27 21:57:05 +0000

    Try again

--- packages/mdx/next.config.js
+++ packages/mdx/next.config.js
@@ -11,7 +11,7 @@ const withMDX = require("@next/mdx")({
 module.exports = withMDX({
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
 
-  async redirects() {
+  redirects() {
     return [
       {
         source: "/",
