1483cfecc455afca79ebf6f3fe18ccd88385f314
Rodrigo Pombo <pombopombopombo@gmail.com>
2022-04-20 12:00:26 +0000

    Test bundle analyzer

--- examples/bundle-test/pages/js-page.js
+++ examples/bundle-test/pages/js-page.js
@@ -2,7 +2,7 @@ export default function Page() {
   return (
     <div>
       <h1>Hello</h1>
-      Lorem ipsum dolor sit amet.
+      Lorem ipsum dolor sit amet. Consectetur adipiscing elit.
     </div>
   )
 }
