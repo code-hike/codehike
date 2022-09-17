c78f3a97f8e8118fa75d14849c1ba6783212a970
John Daly <john.daly2@gmail.com>
2022-09-17 16:17:06 -0700

    Adding @code-hike/mdx and configuring it

--- examples/astro/astro.config.mjs
+++ examples/astro/astro.config.mjs
@@ -1,4 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
+import react from "@astrojs/react"
+import mdx from "@astrojs/mdx"
+import { remarkCodeHike } from "@code-hike/mdx"
+import theme from "shiki/themes/github-dark.json"
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  markdown: {
+    syntaxHighlight: false,
+  },
+  integrations: [
+    react(),
+    mdx({ remarkPlugins: [[remarkCodeHike, { autoImport: false, theme }]] }),
+  ],
+})
