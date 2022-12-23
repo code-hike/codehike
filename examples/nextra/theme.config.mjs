3a1754f95f3738f73d96a430f4a4c0b94eff5eba
Rodrigo Pombo <pombopombopombo@gmail.com>
2022-12-23 13:50:26 +0000

    Remove ch from nextra for now

--- examples/nextra/theme.config.mjs
+++ examples/nextra/theme.config.mjs
@@ -1,10 +1,10 @@
-import { remarkCodeHike } from "@code-hike/mdx"
-import theme from "shiki/themes/nord.json"
+// import { remarkCodeHike } from "@code-hike/mdx"
+// import theme from "shiki/themes/nord.json"
 
 export default {
   logo: <span>My Nextra Documentation</span>,
   project: {
     link: "https://github.com/shuding/nextra",
   },
-  remarkPlugins: [[remarkCodeHike, { theme }]],
+  // remarkPlugins: [[remarkCodeHike, { theme }]],
 }
