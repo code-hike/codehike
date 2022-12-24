352282dc5252101f307622a2bb33d378c3f1965f
Rodrigo Pombo <pombopombopombo@gmail.com>
2022-12-24 14:07:44 +0000

    Fix nextra

--- examples/nextra/next.config.mjs
+++ examples/nextra/next.config.mjs
@@ -1,8 +1,13 @@
 import nextra from "nextra"
+import { remarkCodeHike } from "@code-hike/mdx"
+import theme from "shiki/themes/nord.json" assert { type: "json" }
 
 const withNextra = nextra({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.mjs",
+  mdxOptions: {
+    remarkPlugins: [[remarkCodeHike, { theme }]],
+  },
 })
 
 export default withNextra()
--- examples/nextra/theme.config.mjs
+++ examples/nextra/theme.config.mjs
@@ -1,10 +1,6 @@
-// import { remarkCodeHike } from "@code-hike/mdx"
-// import theme from "shiki/themes/nord.json"
-
 export default {
   logo: <span>My Nextra Documentation</span>,
   project: {
     link: "https://github.com/shuding/nextra",
   },
-  // remarkPlugins: [[remarkCodeHike, { theme }]],
 }
