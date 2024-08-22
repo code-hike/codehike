73cc88b5fc3bd2ab25bddf1e02898d1a9059d3f5
Rodrigo Pombo <pombopombopombo@gmail.com>
2024-08-22 15:20:11 +0200

    Update urls

--- apps/web/app/blog/feed.xml/route.ts
+++ apps/web/app/blog/feed.xml/route.ts
@@ -10,19 +10,19 @@ export async function GET() {
     title: "Code Hike",
     description: "Markdown, React and everything in between.",
     generator: "RSS for Node and Next.js",
-    feed_url: "https://v1.codehike.org/blog/feed.xml",
-    site_url: "https://v1.codehike.org/blog",
+    feed_url: "https://codehike.org/blog/feed.xml",
+    site_url: "https://codehike.org/blog",
     language: "en-US",
     pubDate: new Date().toUTCString(),
     ttl: 120,
-    image_url: "https://v1.codehike.org/logo.png",
+    image_url: "https://codehike.org/logo.png",
   })
 
   posts.map(({ data, url }) => {
     feed.item({
       title: data.title,
       description: data.description || "",
-      url: `https://v1.codehike.org${url}`,
+      url: `https://codehike.org${url}`,
       author: data.authors[0],
       date: data.date,
     })
